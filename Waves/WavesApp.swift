140ce11c94a17cf285c8ff89436dc23633519ddb
Sahil Mahendrakar <sahil.mahendrakar@gmail.com>
2026-02-28 11:54:25 -0500

    First commit

---
+++ Waves/WavesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WavesApp.swift
+//  Waves
+//
+//  Created by Sahil Mahendrakar on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct WavesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
